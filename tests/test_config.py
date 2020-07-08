632803c310edee24d8d3a0fd5f8346350af1d616
JSextonn <justinsexton.dev@gmail.com>
2020-07-08 17:40:06 -0500

    Added additional env_var test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 import pytest
 
-from pyocle import config
+import pyocle
+from pyocle.config import MissingEnvironmentVariableError
 
 
 @pytest.fixture
@@ -11,6 +12,16 @@ def env():
 
 
 def test_env_var_is_retrieved_correctly(env):
-    environment_variable = config.env_var('CONNECTION_STRING', environment=env)
+    environment_variable = pyocle.config.env_var('CONNECTION_STRING', environment=env)
     assert environment_variable is not None
     assert environment_variable == 'connect'
+
+
+def test_env_var_raises_error_when_variable_does_not_exist(env):
+    missing_variable = 'does not exist'
+    with pytest.raises(MissingEnvironmentVariableError) as exception_info:
+        pyocle.config.env_var(missing_variable, environment=env)
+
+    exception = exception_info.value
+    assert exception.env_var_name == missing_variable
+    assert str(exception) == f'An environment variable with the name: {missing_variable} could not be found.'
